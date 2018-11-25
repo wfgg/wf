ced2f7e4fe58c03d1f68f9ce8bdd101c00183c5a
wfgg <43932345+wfgg@users.noreply.github.com>
2018-11-25 15:00:56 +0800

    Update app_version.php

--- app_version.php
+++ app_version.php
@@ -5,5 +5,5 @@
 	"apkUrl":"http://www.lofter.com/rsc/android/lofter.apk",
 	"required":false,
 	"releaseDate":"2018-02-12 12:45:20",
-	"releaseNotes":["新版本特性新版本特性新版本特性；", "描述版本信息，方便用户选择是否立即下载更新；", "性能优化和BUG修复。"]
+	"releaseNotes":["新版本特性新版本特性新版本特性1；", "描述版本信息，方便用户选择是否立即下载更新；", "性能优化和BUG修复。"]
 }
